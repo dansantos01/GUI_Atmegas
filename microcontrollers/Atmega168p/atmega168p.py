5db9c4550c417512cd89d21f6dfa9e7825883351
Diogo Santos <1190501@isep.ipp.pt>
2023-09-29 20:36:33 +0100

    Remade Timer0 and Timer2 168p

--- microcontrollers/Atmega168p/atmega168p.py
+++ microcontrollers/Atmega168p/atmega168p.py
@@ -1,6 +1,6 @@
 from microcontrollers.Atmega168p.Modules.timer_counter_0.timer0_codegen import *
 from microcontrollers.Atmega168p.Modules.timer_counter_1.timer1_module import *
-from microcontrollers.Atmega168p.Modules.timer_counter_2.timer2_module import *
+from microcontrollers.Atmega168p.Modules.timer_counter_2.timer2_codegen import *
 from microcontrollers.Atmega168p.Modules.analog_to_digital_converter.adc_codegen import *
 from microcontrollers.Atmega168p.Modules.usart_0.usart0_codegen import *
 from kivy.uix.screenmanager import ScreenManager, Screen
